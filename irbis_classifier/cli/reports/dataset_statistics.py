3e9da8dc25fee258c33d16be6b6a2f87cf044088
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-01-24 11:59:48 +0300

    reports cli and src lil stype update

--- irbis_classifier/cli/reports/dataset_statistics.py
+++ irbis_classifier/cli/reports/dataset_statistics.py
@@ -6,14 +6,14 @@ import click
 
 from irbis_classifier.src.plots import (
     create_pie_plots_over_split,
-    create_classes_difference_over_split,
-    create_bar_plot_over_stages,
+    create_classes_difference_bar_plot_over_split,
+    create_classes_bar_plot_over_stages,
 )
 
 
 @click.command()
 @click.option('--path_to_data_dir', type=click.Path(exists=True), help='The path to the data directory')
-@click.option('--path_to_save_dir', type=click.Path(exists=True), help='The path to the data directory')
+@click.option('--path_to_save_dir', type=click.Path(), help='The path to the data directory')
 def collect_figures(
     path_to_data_dir: Path | str,
     path_to_save_dir: Path | str,
@@ -28,14 +28,14 @@ def collect_figures(
         path_to_save_dir / 'splits_classes_distribution',
     )
 
-    create_classes_difference_over_split(
+    create_classes_difference_bar_plot_over_split(
         path_to_data_dir,
         False,
         True,
         path_to_save_dir,
     )
 
-    create_bar_plot_over_stages(
+    create_classes_bar_plot_over_stages(
         path_to_data_dir,
         False,
         True,

f41014852b63f54c1de0001b0f24a7f41799381f
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-02-06 14:31:22 +0300

    dataset upadted: now it can work well with grayscale images

--- irbis_classifier/src/training/datasets.py
+++ irbis_classifier/src/training/datasets.py
@@ -30,9 +30,15 @@ class AnimalDataset(Dataset):
         index: int
     ) -> tuple[torch.Tensor, int]:
         current_serie: pd.Series = self._df.loc[index]
-        image_np: np.ndarray = np.asarray(Image.open(current_serie.path))
 
-        height, width, _ = image_np.shape
+        image_np: np.ndarray = np.asarray(Image.open(current_serie.path))
+        image_shape = image_np.shape
+
+        if len(image_shape) == 3:
+            height, width, _ = image_shape
+        else:
+            height, width = image_shape
+            image_np = image_np[..., None].repeat(3, -1)
 
         x_lower_left: int = round((current_serie.x_center - current_serie.width / 2) * width)
         x_lower_left = max(x_lower_left, 0)
@@ -44,6 +50,5 @@ class AnimalDataset(Dataset):
 
         crop = image_np[y_lower_left:y_upper_right, x_lower_left:x_upper_right, :]
         crop = self._transforms(image=crop)['image']
-        crop = crop / 255
 
         return crop, current_serie.class_id

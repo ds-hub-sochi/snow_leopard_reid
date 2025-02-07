9fdfb196fd3d4e437f39035fffe62bcddbce7daa
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-02-07 14:53:46 +0300

    experimet creating replaced to specific function

---
+++ irbis_classifier/src/training/experiment.py
@@ -0,0 +1,35 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path
+
+from comet_ml import CometExperiment, start
+
+
+@dataclass
+class ExperimentConfig:
+    api_key: str
+    project_name: str
+    workspace: str
+
+
+def setup_experimet(
+    path_to_json_config: str | Path,
+    run_name: str,
+):
+    with open(
+        path_to_json_config,
+        'r',
+        encoding='utf-8',
+    ) as json_config:
+        experiment_config: ExperimentConfig = ExperimentConfig(
+            **json.load(json_config)
+        )
+
+    expetiment: CometExperiment = start(
+        api_key=experiment_config.api_key,
+        project_name=experiment_config.project_name,
+        workspace=experiment_config.workspace,
+    )
+    expetiment.set_name(run_name)
+
+    return expetiment

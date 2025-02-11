87ce06ef87927eeb752135e21ddc4f5332e3abba
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-02-11 15:39:47 +0300

    training cli updated: added label_encoder; removed code for testng

--- irbis_classifier/cli/training/start_training.py
+++ irbis_classifier/cli/training/start_training.py
@@ -15,6 +15,8 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import models
 
+from irbis_classifier.src.label_encoder import create_label_encoder, LabelEncoder
+from irbis_classifier.src.plots import create_barplot_with_confidence_intervals
 from irbis_classifier.src.training import (
     create_train_val_test_datasets,
     setup_experimet,
@@ -22,6 +24,8 @@ from irbis_classifier.src.training import (
     train_transforms,
     val_transfroms,
 )
+from irbis_classifier.src.testing.testers import MetricsEstimations
+from irbis_classifier.src.testing.utils import test_model
 
 
 @click.command()
@@ -32,26 +36,56 @@ from irbis_classifier.src.training import (
     help='path to dir where model checkpoint will be stored',
 )
 @click.option('--path_to_experiment_config', type=click.Path(exists=True), help='path to experoment config json file')
+@click.option('--run_name', type=str, help='name of a run in the comet reports')
 @click.option('--batch_size', type=int, help='batch size you want to use; please note that DataParallel is used')
 @click.option('--n_epochs', type=int, help='the duration of training in epochs')
 @click.option('--lr', type=float, help='learning rate you want to setup for your optimize; exponential way is OK')
 @click.option('--device_ids', type=str, help='ids of the devices you want to as a comma separated string; ex. "0,1"')
+@click.option(
+    '--path_to_unification_mapping_json',
+    type=click.Path(exists=True),
+    help='The path to the json file with unification mapping',
+)
+@click.option(
+    '--path_to_supported_labels_json',
+    type=click.Path(exists=True),
+    help='The path to the json file with the list of supported labels',
+)
+@click.option(
+    '--path_to_russian_to_english_mapping_json',
+    type=click.Path(exists=True),
+    help='The path to the json file with the russian to english mapping',
+)
 def start_training(  # pylint: disable=too-many-positional-arguments,too-many-locals
     path_to_data_dir: str | Path,
     path_to_checkpoints_dir: str | Path,
     path_to_experiment_config: str | Path,
+    run_name: str,
     batch_size: int,
     n_epochs: int,
     lr: float,
     device_ids: str,
+    path_to_unification_mapping_json: Path | str,
+    path_to_supported_labels_json: Path | str,
+    path_to_russian_to_english_mapping_json: Path | str,
 ):
     path_to_data_dir = Path(path_to_data_dir).resolve()
+
+    path_to_unification_mapping_json = Path(path_to_unification_mapping_json).resolve()
+    path_to_supported_labels_json = Path(path_to_supported_labels_json).resolve()
+    path_to_russian_to_english_mapping_json = Path(path_to_russian_to_english_mapping_json).resolve()
 
     path_to_checkpoints_dir = Path(path_to_checkpoints_dir).resolve()
     path_to_checkpoints_dir = path_to_checkpoints_dir / datetime.today().strftime('%Y-%m-%d')
     path_to_checkpoints_dir.mkdir(
         parents=True,
         exist_ok=True,
+    )
+
+    label_encoder: LabelEncoder = create_label_encoder(
+        path_to_unification_mapping_json,
+        path_to_supported_labels_json,
+        path_to_russian_to_english_mapping_json,
     )
 
     try:
@@ -61,7 +95,6 @@ def start_training(  # pylint: disable=too-many-positional-arguments,too-many-lo
 
         return
 
-    run_name: str = 'test'
     try:
         experiment: comet_ml.CometExperiment = setup_experimet(
             path_to_experiment_config,
@@ -78,17 +111,17 @@ def start_training(  # pylint: disable=too-many-positional-arguments,too-many-lo
         val_transforms=val_transfroms,
     )
 
-    val_dataloader: DataLoader = DataLoader(
-        val_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=os.cpu_count(),
-    )
-
     train_dataloader: DataLoader = DataLoader(
         train_dataset,
         batch_size=batch_size,
         shuffle=True,
+        num_workers=os.cpu_count(),
+    )
+
+    val_dataloader: DataLoader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
         num_workers=os.cpu_count(),
     )
 
@@ -105,6 +138,11 @@ def start_training(  # pylint: disable=too-many-positional-arguments,too-many-lo
     )
     model = model.to(device)
 
+    if isinstance(model, nn.DataParallel):
+        model_label: str = model.module.__class__.__name__
+    else:
+        model_label = model.__class__.__name__
+
     optimizer: torch.optim.Optimizer = torch.optim.Adam(
         params=model.parameters(),
         lr=lr,
@@ -119,6 +157,7 @@ def start_training(  # pylint: disable=too-many-positional-arguments,too-many-lo
         'initial lerning rate': lr,
         'architecture': model.module.__class__.__name__,
     }
+
     experiment.log_parameters(parameters)
 
     trainer: Trainer = Trainer(
@@ -136,6 +175,8 @@ def start_training(  # pylint: disable=too-many-positional-arguments,too-many-lo
         val_dataloader,
         device,
         experiment,
+        model_label,
+        label_encoder,
     )
 
     experiment.end()

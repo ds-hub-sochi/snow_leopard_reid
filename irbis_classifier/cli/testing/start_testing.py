78c743a2a4db31fd64d6387ea1acf94bed468105
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-03-14 13:17:00 +0300

    testing cli updated with config instead of cli args

--- irbis_classifier/cli/testing/start_testing.py
+++ irbis_classifier/cli/testing/start_testing.py
@@ -13,147 +13,108 @@ from torch.utils.data import DataLoader
 from irbis_classifier.src.label_encoder import create_label_encoder, LabelEncoder
 from irbis_classifier.src.plots import create_barplot_with_confidence_intervals, create_confusion_matrix
 from irbis_classifier.src.testing.test import test_model
-from irbis_classifier.src.testing.testers import MetricsEstimations
 from irbis_classifier.src.training.datasets import AnimalDataset
 from irbis_classifier.src.training.transforms import get_val_transforms
 
 
 @dataclass
-class MetricsJson:
-    averaged_f1_macro: MetricsEstimations
-    f1_for_classes: dict[str, MetricsEstimations]
+class LabelEncoderParams:
+    path_to_unification_mapping_json: str | Path
+    path_to_supported_labels_json: str | Path
+    path_to_russian_to_english_mapping_json: str | Path
+
+    def __post_init__(self):
+        self.path_to_unification_mapping_json = Path(self.path_to_unification_mapping_json).resolve()
+        self.path_to_supported_labels_json = Path(self.path_to_supported_labels_json).resolve()
+        self.path_to_russian_to_english_mapping_json = Path(self.path_to_russian_to_english_mapping_json).resolve()
+
+@dataclass
+class Normalization:
+    mean: list[float]
+    std: list[float]
+
+@dataclass
+class ImageResizing:
+    size_before_padding: int
+    size_after_padding: int
+
+@dataclass
+class ConfidenceIntervalParams:
+    bootstrap_size: int
+    alpha: float
+
+@dataclass
+class TestingConfig:
+    path_to_test_csv: str | Path
+    path_to_traced_model: str | Path
+    path_to_save_dir: str
+    batch_size: int
+    label_encoder_params: LabelEncoderParams
+    normalization: Normalization
+    image_resizing: ImageResizing
+    metrics_to_use: list[str]
+    confidence_interval_params: ConfidenceIntervalParams
+
+    def __post_init__(self):
+        self.path_to_test_csv = Path(self.path_to_test_csv).resolve()
+
+        self.path_to_save_dir = Path(self.path_to_save_dir).resolve()
+        self.path_to_save_dir = self.path_to_save_dir / self.path_to_traced_model[:-3]
+        self.path_to_save_dir.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+
+        self.label_encoder_params = LabelEncoderParams(**self.label_encoder_params)
+        
+        self.normalization = Normalization(**self.normalization)
+        
+        self.image_resizing = ImageResizing(**self.image_resizing)
+
+        self.confidence_interval_params = ConfidenceIntervalParams(**self.confidence_interval_params)
 
 
 @click.command()
 @click.option(
-    '--path_to_test_csv',
+    '--path_to_config',
     type=click.Path(exists=True),
-    help='path to csv-file with test data',
+    help='path to the json-config file',
 )
-@click.option(
-    '--path_to_traced_model',
-    type=click.Path(exists=True),
-    help ="path to traced model dump",
-)
-@click.option(
-    '--batch_size',
-    type=int,
-    help ="batch size to use; better be training batch size / number of gpus")
-@click.option(
-    '--bootstrap_size',
-    type=int,
-    help='size of a bootstrapped sample',
-)
-@click.option(
-    '--alpha',
-    type=float, 
-    help='required confidence level',
-)
-@click.option(
-    '--path_to_save_dir',
-    type=click.Path(), 
-    help='the path to the data directory',
-)
-@click.option(
-    '--path_to_unification_mapping_json',
-    type=click.Path(exists=True),
-    help='the path to the json file with unification mapping',
-)
-@click.option(
-    '--path_to_supported_labels_json',
-    type=click.Path(exists=True),
-    help='the path to the json file with the list of supported labels',
-)
-@click.option(
-    '--path_to_russian_to_english_mapping_json',
-    type=click.Path(exists=True),
-    help='the path to the json file with the russian to english mapping',
-)
-@click.option(
-    '--mean',
-    type=str,
-    default='0.485,0.456,0.406',
-    help='normalization mean; better see model description for the proper values',
-)
-@click.option(
-    '--std',
-    type=str,
-    default='0.229,0.224,0.225',
-    help='normalization standart deviation; better see model description for the proper values',
-)
-@click.option(
-    '--max_size',
-    type=int,
-    default=256,
-    help='max image size; bigger side will be resized to this size',
-)
-@click.option(
-    '--resize',
-    type=int,
-    default=224,
-    help='after the padding applied image will be resized to this size',
-)
-def run_testing(  # pylint: disable=too-many-positional-arguments,too-many-arguments,too-many-locals
-    path_to_test_csv: str | Path,
-    path_to_traced_model: str,
-    batch_size: int,
-    bootstrap_size: int,
-    alpha: float,
-    path_to_save_dir: str | Path,
-    path_to_unification_mapping_json: Path | str,
-    path_to_supported_labels_json: Path | str,
-    path_to_russian_to_english_mapping_json: Path | str,
-    mean='0.485,0.456,0.406',
-    std='0.229,0.224,0.225',
-    max_size: int = 256,
-    resize: int = 224,
+def run_testing(
+    path_to_config: str | Path,
 ) -> None:
-    path_to_test_csv = Path(path_to_test_csv).resolve()
-
-    path_to_save_dir = Path(path_to_save_dir).resolve()
-    path_to_save_dir = path_to_save_dir / path_to_traced_model[:-3]
-    path_to_save_dir.mkdir(
-        parents=True,
-        exist_ok=True,
-    )
-
-    path_to_unification_mapping_json = Path(path_to_unification_mapping_json).resolve()
-    path_to_supported_labels_json = Path(path_to_supported_labels_json).resolve()
-    path_to_russian_to_english_mapping_json = Path(path_to_russian_to_english_mapping_json).resolve()
+    with open(
+        path_to_config,
+        'r',
+        encoding='utf-8',
+    ) as json_file:
+        config: TestingConfig = TestingConfig(**json.load(json_file))
 
     label_encoder: LabelEncoder = create_label_encoder(
-        path_to_unification_mapping_json,
-        path_to_supported_labels_json,
-        path_to_russian_to_english_mapping_json,
+        path_to_unification_mapping_json=config.label_encoder_params.path_to_russian_to_english_mapping_json,
+        path_to_supported_classes_json=config.label_encoder_params.path_to_supported_labels_json,
+        path_to_russian_to_english_mapping_json=config.label_encoder_params.path_to_russian_to_english_mapping_json,
     )
-
-    try:
-        mean_lst: list[float] = [float(value) for value in mean.split(',')]
-        std_lst: list[float] = [float(value) for value in std.split(',')]
-    except ValueError:
-        logger.error('check mean and std you have passed; it most be a comma separated string like "0.1,0.2,0.3"')
-
-        return
 
     device: torch.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    model: torch.jit.ScriptModule = torch.jit.load(path_to_traced_model)
+    model: torch.jit.ScriptModule = torch.jit.load(config.path_to_traced_model)
     model.to(device)
     model.eval()
 
     test_dataset: AnimalDataset = AnimalDataset(
-        path_to_test_csv,
+        config.path_to_test_csv,
         get_val_transforms(
-            mean=mean_lst,
-            std=std_lst,
-            max_size=max_size,
-            resize=resize,
+            mean=config.normalization.mean,
+            std=config.normalization.std,
+            max_size=config.image_resizing.size_before_padding,
+            resize=config.image_resizing.size_after_padding,
         ),
     )
 
     test_dataloader: DataLoader = DataLoader(
         test_dataset,
-        batch_size=batch_size,
+        batch_size=config.batch_size,
         shuffle=False,
         num_workers=os.cpu_count(),
     )
@@ -168,8 +129,8 @@ def run_testing(  # pylint: disable=too-many-positional-arguments,too-many-argum
         test_dataloader,
         model,
         metrics,
-        bootstrap_size,
-        alpha,
+        config.confidence_interval_params.bootstrap_size,
+        config.confidence_interval_params.alpha,
     )
 
     for normalization in ('over actual', 'over predicted'):
@@ -179,7 +140,7 @@ def run_testing(  # pylint: disable=too-many-positional-arguments,too-many-argum
             normalize=normalization,
             show=False,
             save=True,
-            save_dir=path_to_save_dir,
+            save_dir=config.path_to_save_dir,
             title=f'Confusion matrix\n(normalized {normalization})'
         )
 
@@ -190,7 +151,7 @@ def run_testing(  # pylint: disable=too-many-positional-arguments,too-many-argum
             1.0,
             show=False,
             save=True,
-            save_dir=path_to_save_dir,
+            save_dir=config.path_to_save_dir,
             metric_name=current_metric_name,
             labels=[label_encoder.get_label_by_index(i) for i in current_metrics_results[1]],
         )
@@ -211,7 +172,7 @@ def run_testing(  # pylint: disable=too-many-positional-arguments,too-many-argum
             )
 
     with open(
-        path_to_save_dir / 'metrics.json',
+        config.path_to_save_dir / 'metrics.json',
         'w',
         encoding='utf-8',
     ) as metrics_file:

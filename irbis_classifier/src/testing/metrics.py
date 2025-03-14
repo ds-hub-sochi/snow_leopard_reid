27dca1194cb43df8873137c9ccddbebe2fe3ea60
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-03-14 10:17:50 +0300

    create_confusion_matrix replaced here; metrci factory created

---
+++ irbis_classifier/src/testing/metrics.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import inspect
+import sys
+from collections.abc import Callable, Sequence
+from typing import Type
+
+import sklearn.metrics as sklearn_metrics
+
+
+def create_confusion_matrix(
+    y_true: Sequence[int],
+    y_predicted: Sequence[int],
+) -> list[list[int]]:
+    assert len(y_true) == len(y_predicted), 'number of predictions must be equal to the number of objects'
+
+    confision_matrix: list[list[int]] = []
+
+    unique_actual_labels = sorted(list(set(y_true)))  # since we have all the classes in the val/test, it's OK
+
+    for _ in unique_actual_labels:
+        confision_matrix.append([0] * len(unique_actual_labels))
+
+    for true_target, predicted_target in zip(y_true, y_predicted):
+        confision_matrix[true_target][predicted_target] += 1
+
+    return confision_matrix
+
+
+class MetricFactory:
+    def get_metrics_funcion(
+        self,
+        metric_name: str,
+    ) -> Type[Callable[[Sequence[int], Sequence[int]], float]]:
+        for object_name, obj in inspect.getmembers(sys.modules[__name__]):
+            if inspect.isfunction(obj) and object_name == metric_name:
+                return obj
+
+        if hasattr(sklearn_metrics, metric_name):
+            return getattr(
+                sklearn_metrics,
+                metric_name,
+            )
+
+        raise ValueError(f"metric you've provided '{metric_name}' wasn't found")

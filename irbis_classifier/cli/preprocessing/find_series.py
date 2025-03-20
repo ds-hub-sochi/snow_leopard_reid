53c17c9d3248412c4d249292d2835102155acb90
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-03-20 12:49:36 +0300

    find series cli updated with the support of old stages only update, not fully reconctruction

--- irbis_classifier/cli/preprocessing/find_series.py
+++ irbis_classifier/cli/preprocessing/find_series.py
@@ -18,9 +18,7 @@ from irbis_classifier.src.series_utils import add_series_info
 
 def construct_series(
     path_to_data_dir: Path,
-    path_to_unification_mapping_json: Path,
-    path_to_supported_labels_json: Path,
-    path_to_russian_to_english_mapping_json: Path,
+    label_encoder: LabelEncoder,
 ) -> pd.DataFrame | None:
     all_image_paths: list[Path] = list(path_to_data_dir.rglob('*.*'))
     all_image_paths = filter_non_images(all_image_paths)
@@ -43,12 +41,6 @@ def construct_series(
 
     df['specie'] = df['specie'].apply(fix_rus_i_naming)
 
-    label_encoder: LabelEncoder = create_label_encoder(
-        path_to_unification_mapping_json,
-        path_to_supported_labels_json,
-        path_to_russian_to_english_mapping_json,
-    )
-
     df['unified_class'] = df['specie'].map(label_encoder.get_unified_label)
     df = df[df['unified_class'].notna()]
 
@@ -70,9 +62,9 @@ def construct_series(
     help='The path to the data directory',
 )
 @click.option(
-    '--stages_to_ignore',
+    '--old_stages',
     type=str,
-    help='stages you want to skip for now. Must me a comma-separated string of ints',
+    help='stages you want to update. Must me a comma-separated string of ints like "1,2"',
 )
 @click.option(
     '--path_to_unification_mapping_json',
@@ -92,7 +84,7 @@ def construct_series(
 def find_series(
     path_to_data_dir: Path | str,
     path_to_save_dir: Path | str,
-    stages_to_ignore: str,
+    old_stages: str,
     path_to_unification_mapping_json: Path | str,
     path_to_supported_labels_json: Path | str,
     path_to_russian_to_english_mapping_json: Path | str,
@@ -108,18 +100,56 @@ def find_series(
         parents=True,
     )
 
-    stages_to_ignore_list: set[str] = {'stage_' + stage_number for stage_number in stages_to_ignore.split(',')}
+    label_encoder: LabelEncoder = create_label_encoder(
+        path_to_unification_mapping_json,
+        path_to_supported_labels_json,
+        path_to_russian_to_english_mapping_json,
+    )
+
+    old_stages_list: set[str] = {'stage_' + stage_number for stage_number in old_stages.split(',')}
+
+    # old stages' labels must be updated if unification mapping was updated
+    stages_to_update: list[str] = list(old_stages_list)
+
+    for stage in stages_to_update:
+        logger.info(f'updating {stage}')
+
+        df: pd.DataFrame = pd.read_csv(
+            path_to_save_dir / f'df_{stage}.csv',
+            index_col = None,
+        )
+
+        df_original: pd.DataFrame = df.copy(deep=True)
+
+        df['unified_class'] = df['specie'].map(label_encoder.get_unified_label)
+        df = df[df['unified_class'].notna()]
+
+        df['class_id'] = df['unified_class'].map(label_encoder.get_index_by_label)
+        df = df[df['class_id'].notna()].reset_index(drop=True)
+
+        if not all(
+            [
+                df[df.class_id.to_numpy() != df_original.class_id.to_numpy()].shape[0] == 0,
+                df_original[df.class_id.to_numpy() != df_original.class_id.to_numpy()].shape[0] == 0,
+            ]
+        ):
+            logger.error(f"During the processing of {stage} some classes' indexes have changed; all the code was stopped")
+            return
+
+        df.to_csv(
+            path_to_save_dir / f'df_{stage}.csv',
+            index=False,
+        )
+        logger.success(f'ended with {stage}')
 
     stages: list[str] = [f.path.split('/')[-1] for f in os.scandir(path_to_data_dir) if Path(path_to_data_dir).is_dir()]
-    stages = [stage for stage in stages if stage not in stages_to_ignore_list]
+    stages = [stage for stage in stages if stage not in old_stages_list]
 
     for stage in stages:
         logger.info(f'processing {stage}')
         current_stage_df: pd.DataFrame | None = construct_series(  # pylint: disable=too-many-function-args
             path_to_data_dir / stage,
-            path_to_unification_mapping_json,
-            path_to_supported_labels_json,
-            path_to_russian_to_english_mapping_json,
+            label_encoder
         )
         if current_stage_df is not None:
             current_stage_df.to_csv(
